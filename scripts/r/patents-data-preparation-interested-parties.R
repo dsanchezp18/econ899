b09df728e757b1a74d6049a36dc320814f14e591
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-04 15:30:59 -0800

    eda of interested parties

--- scripts/r/patents-data-preparation-interested-parties.R
+++ scripts/r/patents-data-preparation-interested-parties.R
@@ -12,6 +12,7 @@
 library(readr)
 library(dplyr)
 library(janitor)
+library(stringr)
 
 # Loading the data --------------------------------------------------------
 
@@ -33,6 +34,7 @@ parties_columns <- c('patent_number' = 'i',
                      'address_l4' = 'c',
                      'address_l5' = 'c',
                      'party_city' = 'c',
+                     'party_province_code' = 'c',
                      'party_province' = 'c',
                      'party_postal_code' = 'c',
                      'party_country_code' = 'c',
@@ -49,5 +51,45 @@ parties_int1 <- read_delim("data/patents/raw/PT_interested_party_1_to_2000000_20
                             col_names = parties_column_names,
                             locale = locale(encoding = "UTF-8"))
 
-spec(parties_int1)
+parties_int2 <- read_delim("data/patents/raw/PT_interested_party_2000001_to_4000000_2023-06-13.csv",
+                            skip = 1,
+                            delim = "|",
+                            col_types = parties_columns,
+                            col_names = parties_column_names,
+                            locale = locale(encoding = "UTF-8"))
 
+# Combine 
+
+parties_int_raw <- bind_rows(parties_int1, parties_int2)
+
+# Clean the data ------------------------------------------------------------
+
+parties_int <- 
+    parties_int_raw %>%
+    mutate(party_country = party_country %>% recode("Unknown" = NA_character_, "Country Unknown" = NA_character_),
+           applicant_type = applicant_type  %>% na_if("-2"))
+
+# EDA ---------------------------------------------------------------------
+
+# Look at patents by applicant type 
+
+parties_by_applicant_type <-
+    parties_int %>%
+    group_by(applicant_type) %>%
+    summarise(n = n()) %>%
+    mutate(prop = n / sum(n))
+
+# Most interested parties are not applicants (makes sense)
+
+# Look at patents by interested party type
+
+# Countries of each interested party
+
+parties_by_country <-
+  parties_int %>%
+  group_by(party_country) %>%
+  summarise(n = n()) %>%
+  mutate(prop = n / sum(n)) %>%
+  arrange(desc(prop))
+
+# Most interested parties are from the US, followed by Canada (without NAs)
--- scripts/r/patents-data-preparation-main.R
+++ scripts/r/patents-data-preparation-main.R
@@ -40,13 +40,13 @@ main_columns <- c('patent_number'= 'i',
                   'filing_country' = 'c',
                   'filing_language' = 'c',
                   'willing_to_sell' = 'integer',
-                  'pct_application_number' = 'c',
-                  'pct_publication_number' = 'c',
-                  'pct_publication_date' = 'D',
+                  'prop_application_number' = 'c',
+                  'prop_publication_number' = 'c',
+                  'prop_publication_date' = 'D',
                   'parent_application_number' = 'integer',
-                  'pct_article' = 'skip',
-                  'pct_section' = 'skip',
-                  'pct_publication_country' = 'c',
+                  'prop_article' = 'skip',
+                  'prop_section' = 'skip',
+                  'prop_publication_country' = 'c',
                   'publication_kind' = 'c',
                   'printed_as_amended_country' = 'c')
 
@@ -81,15 +81,15 @@ patents_main <- bind_rows(patents_main1, patents_main2)
 patents_main %>%
   group_by(application_status_code) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
-# PCT applications (international patent applications)
+# prop applications (international patent applications)
 
 patents_main %>%
   group_by(application_type_code) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Country of publication
@@ -97,7 +97,7 @@ patents_main %>%
 patents_main %>%
   group_by(country_publication) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Document kind type
@@ -105,7 +105,7 @@ patents_main %>%
 patents_main %>%
   group_by(document_kind) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Filing country
@@ -113,7 +113,7 @@ patents_main %>%
 patents_main %>%
   group_by(filing_country) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Not all are initially filed in Canada, but all are published in Canada.
@@ -124,17 +124,17 @@ patents_main %>%
   mutate(has_parent = if_else(is.na(parent_application_number), 'No', 'Yes'))  %>% 
   group_by(has_parent) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Most don't have a parent
 
-# PCT publication country 
+# prop publication country 
 
 patents_main %>%
-  group_by(pct_publication_country) %>%
+  group_by(prop_publication_country) %>%
   summarise(n = n()) %>%
-  mutate(pct = n / sum(n)) %>%
+  mutate(prop = n / sum(n)) %>%
   arrange(desc(n))
 
 # Chart the distribution of the number of patents by month-year of filing date
