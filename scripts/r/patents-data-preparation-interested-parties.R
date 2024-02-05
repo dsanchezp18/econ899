971354ef6f1f10477501e2813694438c3c6f2bfa
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-04 16:44:44 -0800

    make province codebook

--- scripts/r/patents-data-preparation-interested-parties.R
+++ scripts/r/patents-data-preparation-interested-parties.R
@@ -14,6 +14,34 @@ library(dplyr)
 library(janitor)
 library(stringr)
 
+# Define the province codes for Canada (as per WIPO ST.3)
+
+province_codebook <- tibble(
+  province_code = c("AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"),
+  province = c("Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Northwest Territories", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon"),
+  province_code_wipo = c("A1", "B1", "M1", "N3", "N1","N2","N4", "N5", "O1", "Q1", "Q2", "S1", "Y1"),
+  country_mapped_to_province = c("Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada", "Canada")
+)
+
+# Define US state codes (as per WIPO ST.3)
+
+state_codebook <- tibble(
+    state_code = c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"),
+    state = c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"),
+    country_mapped_to_province= rep("United States", 50),
+)
+
+# Define a state-province codebook for all provinces
+
+state_province_codebook <-
+    state_codebook %>%
+    transmute(country_mapped_to_province,
+              province = state, 
+              province_code = state_code,
+              province_code_wipo = province_code)  %>% 
+    bind_rows(province_codebook)  %>% 
+    relocate(country_mapped_to_province)
+
 # Loading the data --------------------------------------------------------
 
 # Define load parameters for loading the interested parties data
@@ -67,7 +95,9 @@ parties_int_raw <- bind_rows(parties_int1, parties_int2)
 parties_int <- 
     parties_int_raw %>%
     mutate(party_country = party_country %>% recode("Unknown" = NA_character_, "Country Unknown" = NA_character_),
-           applicant_type = applicant_type  %>% na_if("-2"))
+           applicant_type = applicant_type  %>% na_if("-2"),
+           party_province_code = party_province_code %>% na_if("-1"))  %>% 
+    left_join(state_province_codebook  %>% select(province_code_wipo, country), by = c("party_province_code" = "province_code_wipo")) 
 
 # EDA ---------------------------------------------------------------------
 
@@ -81,7 +111,16 @@ parties_by_applicant_type <-
 
 # Most interested parties are not applicants (makes sense)
 
-# Look at patents by interested party type
+# Look at patents by interested party code
+
+parties_by_party_code <-
+    parties_int %>%
+    group_by(interested_party_type_code) %>%
+    summarise(n = n()) %>%
+    mutate(prop = n / sum(n)) %>%
+    arrange(desc(prop))
+
+# Most are inventors, then the patent owners. 
 
 # Countries of each interested party
 
@@ -93,3 +132,26 @@ parties_by_country <-
   arrange(desc(prop))
 
 # Most interested parties are from the US, followed by Canada (without NAs)
+
+# Look at provinces of each interested party
+
+parties_by_province <-
+  parties_int %>%
+  group_by(party_province) %>%
+  summarise(n = n()) %>%
+  mutate(prop = n / sum(n)) %>%
+  arrange(desc(prop))
+
+# Only about 7% belong to a province.
+
+# Look at province codes of each interested party
+
+parties_by_province_code <-
+    parties_int %>%
+    group_by(party_province_code) %>%
+    summarise(n = n()) %>%
+    mutate(prop = n / sum(n)) %>%
+    ungroup()  %>% 
+    left_join(province_codebook  %>% select(province_code_wipo, country), by = c("party_province_code" = "province_code_wipo")) %>%
+    filter(country == 'Canada')  %>% 
+    arrange(desc(prop)) 
