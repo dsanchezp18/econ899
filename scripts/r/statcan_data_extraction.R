d14156656e03f05d67a4e3bce5d4ba63e7fe243b
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-11 12:26:46 -0800

    start the statcan data extraction process

--- scripts/r/statcan_data_extraction.R
+++ scripts/r/statcan_data_extraction.R
@@ -11,14 +11,44 @@
 # Load packages
 
 library(dplyr)
+library(readr)
 library(statcanR)
+library(janitor)
+library(tidyr)
+
+# Load province codebook
+
+provinces <- read_csv("data/other/province_codebook.csv", show_col_types = F)
 
 # Labour Force Survey (LFS) -----------------------------------------------------------
 
 # Extract and prepare variables from the Labour Force Survey (LFS)
 
-## By province and month -----------------------------------------------------------
+## Labour force characteristics by province, monthly, seasonally adjusted -----------------------------------------------------------
 
-# Population estimates -----------------------------------------------------------
+# Extract the table using statcanR and clean names
 
-# Extract and prepare variables from the Population Estimates
+lfs_lfc_prov_monthly <- 
+    statcan_download_data("14-10-0287-03", "eng")  %>% 
+    clean_names()
+
+# Prepare working age population estimates (province-month). Consider total pop and also females males. All age groups. 
+
+lfs_pop_prov_monthly_all_genders <- 
+    lfs_lfc_prov_monthly  %>%
+    filter(labour_force_characteristics == "Population",
+           age_group == "15 years and over",
+           data_type == "Seasonally adjusted")  %>% 
+    select(date = ref_date, 
+           geo,
+           scale = scalar_factor, 
+           value,
+           sex)  %>%
+    pivot_wider(names_from = sex,
+                values_from = value,
+                names_prefix = "pop")  %>% 
+    clean_names() %>% 
+    rename(total_pop = pop_both_sexes)  %>% 
+    left_join(provinces %>% select(province, province_code), by = c("geo" = "province"))  %>%
+    relocate(province_code, .after = geo)  %>%
+    arrange(desc(date), geo)
