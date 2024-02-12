41081bc0dc370a91c18408b00eb0bb5c45de10ed
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-12 00:27:25 -0800

    IPC stuff and data sources table

--- scripts/r/data_sources_table.R
+++ scripts/r/data_sources_table.R
@@ -5,3 +5,17 @@
 # Spring 2024 
 
 # This script prepares a table with the data sources used in the paper. 
+
+# Preliminaries -----------------------------------------------------------
+
+# Load libraries 
+
+library(readxl)
+library(dplyr)
+library(modelsummary)
+
+# Read data ----------------------------------------------------------------
+
+# Read the XL file with the data sources table data
+
+data_sources <- read_excel("C:\\Users\\user\\OneDrive\\Documentos\\sfu\\ECON899 MA Paper\\AITC\\Variables.xlsx")
