55b4faa84cc06c91aed3779b900b523aca778521
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-12 00:39:30 -0800

    show data sources table with tinytable

--- scripts/r/data_sources_table.R
+++ scripts/r/data_sources_table.R
@@ -12,10 +12,31 @@
 
 library(readxl)
 library(dplyr)
-library(modelsummary)
+library(kableExtra)
+library(tinytable) # Install using remotes::install_github("vincentarelbundock/tinytable")
+library(janitor)
 
 # Read data ----------------------------------------------------------------
 
 # Read the XL file with the data sources table data
 
-data_sources <- read_excel("C:\\Users\\user\\OneDrive\\Documentos\\sfu\\ECON899 MA Paper\\AITC\\Variables.xlsx")
+data_sources <- 
+    read_excel("C:\\Users\\user\\OneDrive\\Documentos\\sfu\\ECON899 MA Paper\\AITC\\Variables.xlsx",
+            sheet = "Variables (MA Paper)")  %>% 
+    clean_names()
+                
+
+# Prepare the data sources table -------------------------------------------
+
+# Select the relevant columns
+
+data_sources_table_df <- 
+    data_sources %>% 
+    select(-citation) %>%
+    rename("Source" = source,
+           "Variable" = variable,
+           "Note" = note)
+    
+# Preliminary presentation of the data sources table with datasummary 
+
+tt(data_sources_table_df)
