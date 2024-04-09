dbdf39a57ec3ef44ab710e3289428e72afc85d49
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-04-08 17:32:23 -0700

    updates

--- scripts/r/analysis/event_studies_parties.R
+++ scripts/r/analysis/event_studies_parties.R
@@ -25,7 +25,7 @@ library(ggfixest, warn.conflicts = FALSE)
 
 # Load full dataset
 
-df_full <- readRDS("data/full_dataset.rds")
+df_full <- readRDS("data/full_dataset_monthly.rds")
 
 # Define treatment start date
 
@@ -60,30 +60,103 @@ df_event_study <-
 # Baseline event study regressions -----------------------------------------------------------
 
 es_baseline_all_parties <- 
-    feols(ln1interested_parties ~ i(periods, treatment_dummy, ref = -1),
+    feols(ln1_interested_parties ~ i(periods, treatment_dummy, ref = -1),
           data = df_event_study,
           fixef = c("province_code", "periods"),
           cluster = ~ province_code + periods)
 
 es_baseline_inventors <-
-    feols(ln1inventors ~ i(periods, treatment_dummy, ref = -1),
+    feols(ln1_inventors ~ i(periods, treatment_dummy, ref = -1),
           data = df_event_study,
           fixef = c("province_code", "periods"),
           cluster = ~ province_code + periods)
 
 es_baseline_applicants <-
-    feols(ln1applicants ~ i(periods, treatment_dummy, ref = -1),
+    feols(ln1_applicants ~ i(periods, treatment_dummy, ref = -1),
           data = df_event_study,
           fixef = c("province_code", "periods"),
           cluster = ~ province_code + periods)
         
 es_baseline_owners <-
-    feols(ln1owners ~ i(periods, treatment_dummy, ref = -1),
+    feols(ln1_owners ~ i(periods, treatment_dummy, ref = -1),
           data = df_event_study,
           fixef = c("province_code", "periods"),
           cluster = ~ province_code + periods)
 
-# Event study plot with ggiplot
+# Defendable controls event study regressions -----------------------------------------------------------
+
+def_controls <- "+ ln_total_pop + ln_total_full_emp + ln_total_median_wage + cpi + ln_exports_all_countries + ln_imports_all_countries + ln_retail_sales + ln_wholesale_sales + ln_manufacturing_sales + ln1_foreign_parties + ln1_business_insolvencies"
+
+es_def_controls_all_parties <- 
+    feols(fml = paste("ln1_interested_parties ~ i(periods, treatment_dummy, ref = -1)", def_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+
+es_def_controls_inventors <-
+    feols(fml = paste("ln1_inventors ~ i(periods, treatment_dummy, ref = -1)", def_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+    
+es_def_controls_applicants <-
+    feols(fml = paste("ln1_applicants ~ i(periods, treatment_dummy, ref = -1)", def_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+
+es_def_controls_owners <-
+    feols(fml = paste("ln1_owners ~ i(periods, treatment_dummy, ref = -1)", def_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+
+# Additional controls -----------------------------------------------------------
+
+extra_controls <- "+ travellers + vehicles + ln_electric_power_generation + ln_average_actual_hours + new_housing_price_index + ln_food_services_receipts + ln_total_avg_tenure"
+
+add_controls <- paste(def_controls, extra_controls)
+
+es_add_controls_all_parties <- 
+    feols(fml = paste("ln1_interested_parties ~ i(periods, treatment_dummy, ref = -1)", add_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+    
+es_add_controls_inventors <-
+    feols(fml = paste("ln1_inventors ~ i(periods, treatment_dummy, ref = -1)", add_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+    
+es_add_controls_applicants <-
+    feols(fml = paste("ln1_applicants ~ i(periods, treatment_dummy, ref = -1)", add_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)
+
+es_add_controls_owners <-
+    feols(fml = paste("ln1_owners ~ i(periods, treatment_dummy, ref = -1)", add_controls) %>% as.formula(),
+          data = df_event_study,
+          fixef = c("province_code", "periods"),
+          cluster = ~ province_code + periods)   
+
+# Event study plots -----------------------------------------------------------
+
+# Periods 
+
+periods_for_plot <- seq(start_period, end_period, by = 6)
+
+# Dates 
+
+dates <- 
+    df_event_study %>%
+    filter(periods %in% periods_for_plot) %>% 
+    pull(month_year) %>%
+    unique() %>% 
+    format("%b-%y")
+
+## Baseline ------------------------------------------------------------
 
 event_studies_baseline <- list(es_baseline_all_parties, es_baseline_inventors, es_baseline_applicants, es_baseline_owners)
 
@@ -92,4 +165,35 @@ ggiplot(event_studies_baseline,
         multi_style = "facet",
         ci.width = 0,
         pt.pch = 0,
-        facet_args = list(ncol = 1, scales = "free_y"))
+        facet_args = list(ncol = 1, scales = "free_y")) + 
+theme_bw() + 
+scale_x_continuous(breaks = periods_for_plot, labels = dates) +
+theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+## Defendable controls ------------------------------------------------------------
+
+event_studies_def_controls <- list(es_def_controls_all_parties, es_def_controls_inventors, es_def_controls_applicants, es_def_controls_owners)
+
+ggiplot(event_studies_def_controls,
+        geom_style = "errorbar",
+        multi_style = "facet",
+        ci.width = 0,
+        pt.pch = 0,
+        facet_args = list(ncol = 1, scales = "free_y")) +
+theme_bw() +
+scale_x_continuous(breaks = periods_for_plot, labels = dates) +
+theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+## Additional controls ------------------------------------------------------------
+
+event_studies_add_controls <- list(es_add_controls_all_parties, es_add_controls_inventors, es_add_controls_applicants, es_add_controls_owners)
+
+ggiplot(event_studies_add_controls,
+        geom_style = "errorbar",
+        multi_style = "facet",
+        ci.width = 0,
+        pt.pch = 0,
+        facet_args = list(ncol = 1, scales = "free_y")) +
+theme_bw() +
+scale_x_continuous(breaks = periods_for_plot, labels = dates) +
+theme(axis.text.x = element_text(angle = 45, hjust = 1))
